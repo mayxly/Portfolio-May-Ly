6ccfa4cff79632cd889e10afa08eea447b2bdb4a
mayxly <may.ly7303@gmail.com>
2022-05-14 15:18:41 -0400

    Readd files.

--- js/main.js
+++ js/main.js
@@ -1,50 +1,49 @@
-//Menu shown/hidden 
-const navMenu = document.getElementById('nav-menu'), 
-    navToggle = document.getElementById('nav-toggle'),
-    navClose = document.getElementById('nav-close')
+//Menu shown/hidden
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 if (navToggle) {
-navToggle.addEventListener('click', () => {
-    navMenu.classList.add('show-menu')
-    })
-} 
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
+}
 
 if (navClose) {
-    navClose.addEventListener('click', () => {
-        navMenu.classList.remove('show-menu')
-    }) 
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 //Menu disapears when menu item is clicked
-const navLink = document.querySelectorAll('.nav_link')
+const navLink = document.querySelectorAll(".nav_link");
 
-function linkAction(){
-    const navMenu = document.getElementById('nav-menu')
-    navMenu.classList.remove('show-menu')
+function linkAction() {
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show-menu");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction))
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 //Make skills dropdown menu
 
-const skillsContent = document.getElementsByClassName('skills_content'),
-    skillsHeader = document.querySelectorAll('.skills_header')
-
+const skillsContent = document.getElementsByClassName("skills_content"),
+  skillsHeader = document.querySelectorAll(".skills_header");
 
 function toggleSkills() {
-    let itemClass = this.parentNode.className
-    for (i = 0; i < skillsContent.length; ++i) {
-        skillsContent[i].className = 'skills_content skills_close'
-    }
-    if (itemClass === 'skills_content skills_close') {
-        this.parentNode.className = 'skills_content skills_open'
-    }
+  let itemClass = this.parentNode.className;
+  for (i = 0; i < skillsContent.length; ++i) {
+    skillsContent[i].className = "skills_content skills_close";
+  }
+  if (itemClass === "skills_content skills_close") {
+    this.parentNode.className = "skills_content skills_open";
+  }
 }
 
 skillsHeader.forEach((el) => {
-    el.addEventListener('click', toggleSkills)
-})
+  el.addEventListener("click", toggleSkills);
+});
 
 //Website visits
 function websiteVisits(response) {
-    document.querySelector("#visits").textContent = response.value;
+  document.querySelector("#visits").textContent = response.value;
 }
